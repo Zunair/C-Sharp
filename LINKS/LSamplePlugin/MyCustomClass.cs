f1de4c05d7b2015860b193d7c600697f4b1a6953
Zunair <zunair@hotmail.com>
2016-06-13 11:55:54 -0400

    Initial Commit

---
+++ LINKS/LSamplePlugin/MyCustomClass.cs
@@ -0,0 +1,92 @@
+﻿using jarvisWPF;
+using System.Speech.Recognition;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.QualityTools.Testing.Fakes;
+
+/// Add reference to jarvisWPF from %APPDATA%\LINKS\Customization\Plugins\jarvisWPF.exe
+
+/// Make sure the functions called from LINKS are public static string.
+/// params can only be string, convert them inside the method.
+/// Methods are not case sensitive.
+/// Same method names are not compatiable in public v2.5.6.2, in future they will be.
+namespace LSamplePlugin
+{
+    
+    public class MyCustomClass
+    {
+        /// Uncomment the follwing if needed.
+        #region Other Methods
+        //static string CurrentPluginDirectory;
+
+        ///// <summary>
+        ///// This method is run on load, so all the needed events are attached automatically.
+        ///// We can initialize other objects here since this method is run when LINKS is started.
+        ///// </summary>
+        //public static void OnLoad()
+        //{
+        //    // Attach events when LINKS recognizes a phrase
+        //    PublicClass.Recognizer.SpeechRecognized += _SpeechRecognized;
+
+        //    // Create plugin directory
+        //    CurrentPluginDirectory = PublicClass.PluginsPath + typeof(MyCustomClass).Assembly.GetName().Name + "\\";
+        //    System.IO.Directory.CreateDirectory(CurrentPluginDirectory);
+        //}
+
+        ///// <summary>
+        ///// This method is called when a speech is recognized.
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //private static async void _SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        //{
+        //    string RecognizedText = e.Result.Text;
+        //    float RecognizedConfidence = e.Result.Confidence;
+        //    DateTime dt = DateTime.Now;
+        //    string filePath = CurrentPluginDirectory + "LINKS_Heard.txt";
+
+        //    using (StreamWriter ErrorStreamWriter = new StreamWriter(new FileStream(filePath, FileMode.Append, FileAccess.Write, FileShare.Write)))
+        //    {
+        //        string StrToWrite = string.Format("{2}\tRecognized@{0}: \t{1}", new object[] { RecognizedConfidence, RecognizedText, dt });
+        //        await ErrorStreamWriter.WriteLineAsync(StrToWrite);
+        //    }
+        //}
+        #endregion
+
+        /// <summary>
+        /// Returns a string to LINKS in the response, action or web request.
+        /// This method can be called from LINKS like this [LSamplePlugin.MyCustomClass.Test]
+        /// </summary>
+        /// <returns>Hello World</returns>        
+        public static string Test1()
+        {
+            return "Hello World";
+        }
+
+        /// <summary>
+        /// Returns a string to LINKS in the response, action or web request.
+        /// This method can be called from LINKS like this [LSamplePlugin.MyCustomClass.Test("Hello World")]
+        /// </summary>
+        /// <param name="phrase"></param>
+        /// <returns>phrase variable</returns>
+        public static string Test2(string phrase)
+        {
+            return phrase;
+        }
+        
+        /// <summary>
+        /// Returns a string to LINKS in the response, action or web request.
+        /// This method can be called from LINKS like this [LSamplePlugin.MyCustomClass.Test("1","2")]
+        /// </summary>
+        /// <param name="x">number</param>
+        /// <param name="y">number</param>
+        /// <returns>x plus y equals to total</returns>
+        public static string Test3(string x, string y)
+        {
+            return string.Format("{0} plus {1} equals to {2}", int.Parse(x), int.Parse(y), int.Parse(x) + int.Parse(y));
+        }
+    }
+}
